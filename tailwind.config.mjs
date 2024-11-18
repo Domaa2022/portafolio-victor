3bcde0609f301c07e42d94f496cba0848021c9f1
Domaa2022 <victormiguelbarahonalicona@gmail.com>
2024-11-18 15:21:23 -0600

    add about me , skills, education, experience and contact me sections

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,6 +5,7 @@ export default {
 		extend: {
 			backgroundImage: {
 				'icon-gradient': 'linear-gradient(to right, #6DDAC7 30%, #15ABFF 90%)',
+				'icon-gradient-bottom' : 'linear-gradient(to bottom, #6DDAC7 30%, #15ABFF 90%)',
 			},
 		},
 	},
