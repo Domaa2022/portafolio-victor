aeb4df4626c2d0532d9b6c85b81cf57acdb5e2a8
Domaa2022 <victormiguelbarahonalicona@gmail.com>
2024-11-19 20:16:23 -0600

    fixing mobile menu

--- src/components/MenuDropdown.jsx
+++ src/components/MenuDropdown.jsx
@@ -1,18 +1,34 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from "react";
 
 const MenuDropdown = () => {
     const [menuOpen, setMenuOpen] = useState(false);
+    const menuRef = useRef(null);
 
     const handleNavigation = (section) => {
         const targetSection = document.getElementById(section);
         if (targetSection) {
             targetSection.scrollIntoView({ behavior: "smooth" });
         }
-        setMenuOpen(false); // Cierra el menú después de navegar
+        setMenuOpen(false);
     };
 
+    const handleClickOutside = (event) => {
+        if (menuRef.current && !menuRef.current.contains(event.target)) {
+            setMenuOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener("mousedown", handleClickOutside);
+
+        return () => {
+
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
     return (
-        <div className="relative md:hidden">
+        <div className="relative md:hidden" ref={menuRef}>
             <button
                 onClick={() => setMenuOpen(!menuOpen)}
                 className="bg-[#EDEDED] rounded-2xl px-4 py-2 text-lg font-medium shadow-md hover:shadow-lg transition-all"
@@ -36,25 +52,25 @@ const MenuDropdown = () => {
             {menuOpen && (
                 <ul className="absolute top-full left-0 w-auto bg-[#EDEDED] rounded-2xl shadow-md p-4 space-y-2 text-lg font-normal">
                     <li
-                        onClick={() => handleNavigation('aboutMe')}
+                        onClick={() => handleNavigation("aboutMe")}
                         className="hover:text-teal-500 cursor-pointer"
                     >
                         About me
                     </li>
                     <li
-                        onClick={() => handleNavigation('skills')}
+                        onClick={() => handleNavigation("skills")}
                         className="hover:text-teal-500 cursor-pointer"
                     >
                         Skills
                     </li>
                     <li
-                        onClick={() => handleNavigation('experiences')}
+                        onClick={() => handleNavigation("experiences")}
                         className="hover:text-teal-500 cursor-pointer"
                     >
                         Experience
                     </li>
                     <li
-                        onClick={() => handleNavigation('educations')}
+                        onClick={() => handleNavigation("educations")}
                         className="hover:text-teal-500 cursor-pointer"
                     >
                         Education
